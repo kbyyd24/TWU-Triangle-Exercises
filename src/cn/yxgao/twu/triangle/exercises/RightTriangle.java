a115074d3bfc4ec35ebf8615575fe6607f5a7419
gaoyx <melo@gaoyuexiang.cn>
2017-07-23 16:19:48 +0800

    finish right triangle graph

---
+++ src/cn/yxgao/twu/triangle/exercises/RightTriangle.java
@@ -0,0 +1,13 @@
+package cn.yxgao.twu.triangle.exercises;
+
+public class RightTriangle {
+  public static void main(String[] args) {
+    int n = Integer.parseInt(args[0]);
+    for (int i = 1; i <= n; i++) {
+      for (int j = 0; j < i; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
