83625befcf21909eade6b859360f9112d0a96523
gaoyx <melo@gaoyuexiang.cn>
2017-07-23 16:17:11 +0800

    finish verical line exercise

---
+++ src/cn/yxgao/twu/triangle/exercises/VerticalLine.java
@@ -0,0 +1,10 @@
+package cn.yxgao.twu.triangle.exercises;
+
+public class VerticalLine {
+  public static void main(String[] args) {
+    int n = Integer.parseInt(args[0]);
+    for (int i = 0; i < n; i++) {
+      System.out.println("*");
+    }
+  }
+}
